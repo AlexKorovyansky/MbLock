6e86c05482dae67bbd2ada991e720f82baf824ac
Konstantin Dravolin <kdravolin@gmail.com>
2013-07-19 14:48:15 +0700

    add python bluetooth server example

---
+++ Raspberry/extra/bluetooth_server.py
@@ -0,0 +1,29 @@
+# pybluez library
+import bluetooth
+
+server_socket = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+client_sockets = []
+
+server_socket.bind(("",bluetooth.PORT_ANY))
+port = server_socket.getsockname()[1]
+uuid = "00001101-0000-1000-8000-00805F9B34FB"
+
+print "Listening for devices..."
+
+# advertise service
+server_socket.listen(1)
+bluetooth.advertise_service( server_socket, "Validation Host",
+    service_id = uuid,
+    service_classes = [ uuid, bluetooth.SERIAL_PORT_CLASS ],
+    profiles = [ bluetooth.SERIAL_PORT_PROFILE ],
+)
+
+# accept incoming connections
+client_sock, client_info = server_socket.accept()
+# client_sockets.append(client_sock)
+print "Accepted Connection from ", client_info
+
+data=client_sock.recv(1024)
+print "received [%s] \n " % data  
+client_sock.close()
+server_socket.close()  
